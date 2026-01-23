e42346004771294efd512af68589beb4c876b959
hover <thihalinn0004@gamil.com>
2026-01-23 16:12:02 +0630

    add gluestack ui

--- app/index.tsx
+++ app/index.tsx
@@ -1,15 +1,10 @@
-import { Text, View } from "react-native";
+import { Center } from "@/components/ui/center";
+import { Text } from "@/components/ui/text";
 
 export default function Index() {
   return (
-    <View
-      style={{
-        flex: 1,
-        justifyContent: "center",
-        alignItems: "center",
-      }}
-    >
-      <Text>Edit app/index.tsx to edit this screen.</Text>
-    </View>
+      <Center className="flex-1">
+        <Text>Hello glue-stack-ui</Text>
+      </Center>
   );
 }
