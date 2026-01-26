43ab181f153b0a04042b06aa01486a5e96e40c98
hover <thihalinn0004@gamil.com>
2026-01-26 20:16:33 +0630

    add glue-stack ui component

--- constant/Color.ts
+++ constant/Color.ts
@@ -1,12 +1,15 @@
 export const Colors = {
   light: {
-    tabBg: "#ffffff",
+    tabBg: "#f9fafb",
     tabActive: "#16a34a",
     tabInactive: "#9ca3af",
   },
   dark: {
-    tabBg: "#000000",
+    tabBg: "#020617",
     tabActive: "#ffffff",
     tabInactive: "#6b7280",
+  },
+  signinPage: {
+    bgColor: "#46EDD5",
   },
 };
